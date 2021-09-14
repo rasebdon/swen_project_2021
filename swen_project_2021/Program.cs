3b0020738408707ad9469757d1d8364291b0d18c
Raphael Dohnalek <r.dohnalek@gmx.at>
2021-09-14 18:48:10 +0200

    Initial VS Project Upload

---
+++ swen_project_2021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace swen_project_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
